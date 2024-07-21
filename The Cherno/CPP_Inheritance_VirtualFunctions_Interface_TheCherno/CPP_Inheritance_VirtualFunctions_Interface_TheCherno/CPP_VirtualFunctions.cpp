3c3369e59f34dff1963af07548399f6bc090f994
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-07-21 02:47:15 +0200

    Interface DONE

--- The Cherno/CPP_Inheritance_VirtualFunctions_Interface_TheCherno/CPP_Inheritance_VirtualFunctions_Interface_TheCherno/CPP_VirtualFunctions.cpp
+++ The Cherno/CPP_Inheritance_VirtualFunctions_Interface_TheCherno/CPP_Inheritance_VirtualFunctions_Interface_TheCherno/CPP_VirtualFunctions.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <string>
 
-class Entity {
+// Iterface 
+class Printable {
+public:
+	virtual std::string GetClassName() = 0;
+};
+
+
+class Entity : public Printable {
 public:
 	virtual std::string GetName() { return "Entity"; }
+	std::string GetClassName() { return "Entity"; }
 };
 
 class Player : public Entity {
@@ -13,6 +21,7 @@ public:
 	Player(const std::string& name)
 		: m_Name(name) {}
 	std::string GetName() override { return m_Name;  }
+	std::string GetClassName() { return "Player"; }
 };
 
 
@@ -20,15 +29,20 @@ void PrintName(Entity* entity) {
 	std::cout << entity->GetName() << std::endl;
 }
 
+void Print(Printable* obj) {
+	std::cout << obj->GetClassName() << std::endl;
+}
 
 int main() {
 
 	Entity* e = new Entity;
-	PrintName(e);
+	//PrintName(e);
 
 	Player* p = new Player("Marci");
-	PrintName(p);
+	//PrintName(p);
 
+	Print(e);
+	Print(p);
 
 	return 0;
 }
